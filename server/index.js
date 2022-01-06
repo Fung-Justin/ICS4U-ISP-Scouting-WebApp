1ad4199da3a833058b6f928f860ed99691b9b6e4
MatthewWong721 <mwong1@bayviewglen.ca>
2022-01-06 03:22:46 -0500

    Set up database

--- server/index.js
+++ server/index.js
@@ -4,11 +4,13 @@ const cors = require('cors');
 
 const app = express();
 
-//MiddleWare
+//Middleware
 
 app.use(bodyParser.json());
 app.use(cors());
 
+const posts = require('./routes/api/posts');
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
