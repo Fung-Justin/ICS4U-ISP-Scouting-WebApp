4588283b627c5868c56321ed5c237477fe07af92
MayaFreedman <76953551+MayaFreedman@users.noreply.github.com>
2022-01-05 21:38:32 -0500

    Added timer and component to test out creating a timer

---
+++ scouting-app/src/views/timer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Timer/>
+    </div>
+</template>
+
+<script>
+import Timer from '@/components/Timer.vue'
+
+export default {
+components:{
+    Timer
+}
+}
+</script>
