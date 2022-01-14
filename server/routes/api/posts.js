93f6136c9e4f20c5ac3779ace7a5bf3262dc043d
MayaFreedman <76953551+MayaFreedman@users.noreply.github.com>
2022-01-13 21:12:19 -0500

    Added sending completed match to database when submitted

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -3,13 +3,13 @@ const mongodb = require('mongodb');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    const posts = await loadPostsCollection();
+router.get('/', async(req, res) => {
+    const posts = await loadMatches();
     res.send(await posts.find({}).toArray());
 });
 
-router.post('/', async (req, res) => {
-    const posts = await loadPostsCollection();
+router.post('/', async(req, res) => {
+    const posts = await loadMatches();
     await posts.insertOne({
         text: req.body.text,
         createdAt: new Date()
@@ -17,20 +17,18 @@ router.post('/', async (req, res) => {
     res.status(201).send();
 })
 
-router.delete('/:id', async (req, res) => {
-    const posts = await loadPostsCollection();
+router.delete('/:id', async(req, res) => {
+    const posts = await loadMatches();
     await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
     res.status(200).send({});
-  });
+});
 
-async function loadPostsCollection(){
-    const client = await mongodb.MongoClient.connect
-    ('mongodb+srv://user:user@cluster.aebgh.mongodb.net/scouting-app?retryWrites=true&w=majority',
-    {
+async function loadMatches() {
+    const client = await mongodb.MongoClient.connect('mongodb+srv://user:user@cluster.aebgh.mongodb.net/scouting-app?retryWrites=true&w=majority', {
         useNewUrlParser: true
     });
 
-    return client.db('vue-express').collection('posts');
+    return client.db('scouting-app').collection('collection');
 }
 
 module.exports = router;
