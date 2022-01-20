ef5252c2a12247ed108691e77c0d281181e7b424
aalguneh <aalguneh@bayviewglen.ca>
2022-01-19 23:15:36 -0500

    Navbar in progress

--- scouting-app/src/App.vue
+++ scouting-app/src/App.vue
@@ -4,13 +4,20 @@
       <div>
         <router-link to="/"><button id="logout-btn" class="btn mb-2 py-2 text-white" type="submit">Logout
               </button></router-link>
+        <div class = "home">
+          <router-link to="/"><i class = "bi bi-house text-white"></i></router-link>
+          <router-link class = "h5 text-white" to="/">Home</router-link>
+        </div>
+        <div class = "links">
+          <router-link class = "h6 text-white" to="/grid">Grid</router-link>
+        </div>
       </div>
     </div>
   </nav>
   <router-view />
 </template>
 
-Change colours of navbar based on design on the landing page. Modern, simplistic, efficient design 
+Check if home button actually takes you home. 
 
 <style scoped>
 #logout-btn {
@@ -19,5 +26,22 @@ Change colours of navbar based on design on the landing page. Modern, simplistic
   border-radius: 25px;
   padding-right: 20px;
   padding-left: 20px;
+}
+.text-white{
+  text-decoration: none;
+}
+.home {
+  padding: 7px 0;
+  display: inline-block;
+}
+.links {
+  padding: 7px 0;
+  display: inline-block;
+  float:right;
+  margin-right: 18px;
+}
+.bi-house{
+  margin-right: 4px;
+  cursor: pointer;
 }
 </style>
