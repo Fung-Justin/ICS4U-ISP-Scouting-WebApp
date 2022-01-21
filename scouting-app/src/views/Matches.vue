b99835633763c7b3a052f6a5bf2b3f101c2e736c
MayaFreedman <76953551+MayaFreedman@users.noreply.github.com>
2022-01-20 23:25:50 -0500

    Click on names in matches now takes you to that team's matches

--- scouting-app/src/views/Matches.vue
+++ scouting-app/src/views/Matches.vue
@@ -25,22 +25,22 @@
           <h6>{{ data.matchNumber }}</h6>
         </td>
         <td class="bg-danger">
-          <h6>{{ data.r1 }}</h6>
+          <h6 v-on:click="this.$router.push(`team?team=${data.r1}`)">{{ data.r1 }}</h6>
         </td>
          <td class="bg-danger">
-          <h6>{{ data.r2 }}</h6>
+          <h6 v-on:click="this.$router.push(`team?team=${data.r2}`)">{{ data.r2 }}</h6>
         </td>
          <td class="bg-danger">
-          <h6>{{ data.r3 }}</h6>
+          <h6 v-on:click="this.$router.push(`team?team=${data.r3}`)">{{ data.r3 }}</h6>
         </td>
          <td class="bg-primary">
-          <h6>{{ data.b1 }}</h6>
+          <h6 v-on:click="this.$router.push(`team?team=${data.b1}`)">{{ data.b1 }}</h6>
         </td>
          <td class="bg-primary">
-          <h6>{{ data.b2 }}</h6>
+          <h6 v-on:click="this.$router.push(`team?team=${data.b2}`)">{{ data.b2 }}</h6>
         </td>
          <td class="bg-primary">
-          <h6>{{ data.b3 }}</h6>
+          <h6 v-on:click="this.$router.push(`team?team=${data.r1}`)">{{ data.b3 }}</h6>
         </td>
          <td class="bg-danger">
           <h6>{{ data.redClimb }}</h6>
