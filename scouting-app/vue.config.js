584b0298f22aaffce71bd3d44a7ba352051f3850
Justin Fung <jfung2@bayviewglen.ca>
2022-01-21 12:09:22 -0500

    More config changes

--- scouting-app/vue.config.js
+++ scouting-app/vue.config.js
@@ -3,15 +3,15 @@ const path = require('path');
 module.exports = { 
     outputDir: path.resolve( __dirname, '../server/public'),
     devServer: { proxy:  { 
-        "^/url": {
+        "/url": {
              target: "http://localhost:5000", 
-             changeOrigin: true,
+             //changeOrigin: true,
               //pathRewrite: { "^/url": "/" } 
             },
               
-              "^/api": {
+              "/api": {
                 target: "http://localhost:5000", 
-                changeOrigin: true,
+                //changeOrigin: true,
                  //pathRewrite: { "^/api": "/api" }
                  } 
 
